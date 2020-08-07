355145a687384bfbbb681f3e14a8aff48393a5ce
Lucas <lucasdtorres92@gmail.com>
2020-08-07 16:49:46 -0300

    favorites and filter

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
 
 import styles from './styles';
 
@@ -15,6 +20,32 @@ function TeacherItem() {
                 <View style={styles.profileInfo}>
                     <Text style={styles.name}>Lucas Dias</Text>
                     <Text style={styles.subject}>Programação</Text>
+                </View>
+            </View>
+
+            <Text style={styles.bio}>
+                Preciso escrever algo aqui para ser um placeholder.
+                {'\n'}
+                {'\n'}
+                Aqui vai um texto não muito curto mas também não muito extenso, apenas grande o suficiente para ser um placeholder legal
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora {'    '}
+                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                </Text>
+
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
+                        {/* <Image source={heartOutlineIcon} /> */}
+                        <Image source={unfavoriteIcon} />
+                    </RectButton>
+
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                    </RectButton>
                 </View>
             </View>
         </View>
